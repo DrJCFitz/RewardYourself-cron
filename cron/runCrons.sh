9c71fdd16bd05bdc519ee3d940a2fc4dca6ae029
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2016-07-21 08:10:58 -0400

    add appropriate pause in between cron task run

--- cron/runCrons.sh
+++ cron/runCrons.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 aws ecs list-task-definitions --family-prefix cron
+# update statuses
 aws ecs run-task --cluster default --task-definition cron:4 --count 1
-sleep 60
+# pause for 10 minutes to allow status script to complete 
+sleep 600
+# update scrape data
 aws ecs run-task --cluster default --task-definition cron:5 --count 1
