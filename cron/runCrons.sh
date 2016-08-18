03742168ec4358790de0c8049d6bfaf3a5a4c4c3
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2016-08-17 20:51:44 -0400

    change cron task because of CPU allocation

--- cron/runCrons.sh
+++ cron/runCrons.sh
@@ -2,8 +2,8 @@
 export AWS_DEFAULT_REGION=us-east-1
 #aws ecs list-task-definitions --family-prefix cron
 # update statuses
-aws ecs run-task --cluster default --task-definition cron:4 --count 1
+aws ecs run-task --cluster default --task-definition cron:7 --count 1
 # pause for 10 minutes to allow status script to complete 
 sleep 600
 # update scrape data
-aws ecs run-task --cluster default --task-definition cron:5 --count 1
+aws ecs run-task --cluster default --task-definition cron:6 --count 1
