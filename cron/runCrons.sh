0e85efc50f676ca5e8e1b067effd3d2b07d234a2
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2016-09-20 18:16:55 -0400

    fix DeleteRequest, add cron to runCrons.sh

--- cron/runCrons.sh
+++ cron/runCrons.sh
@@ -7,3 +7,6 @@ aws ecs run-task --cluster default --task-definition cron:7 --count 1
 sleep 600
 # update scrape data
 aws ecs run-task --cluster default --task-definition cron:6 --count 1
+sleep 60
+# remove interim data points
+aws ecs run-task --cluster default --task-definition cron:8 --count 1
